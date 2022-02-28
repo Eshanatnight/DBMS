55dcc78ce51c17e73ae2fc5f60bc46755a22b95b
Eshan Chatterjee <eshan2001221@gmail.com>
2022-02-28 18:57:37 +0530

    Working A4

---
+++ Assignment 4/Assignment4.sql
@@ -0,0 +1,4 @@
+-- Find Name of the Subject For Students of CSE 5th Semester
+SELECT SubjectName FROM Subject WHERE Semester='SEM5';
+
+-- Find the Roll Numbers who got marks between 61 and 70 in CS501
